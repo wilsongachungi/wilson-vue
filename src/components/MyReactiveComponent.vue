91ac07afb973c7545d3541dbd0854ec8906cdee7
wilsongachungi <gachungiwilson91@gmail.com>
2024-09-24 20:53:48 +0300

    adding number more than one

--- src/components/MyReactiveComponent.vue
+++ src/components/MyReactiveComponent.vue
@@ -1,10 +1,14 @@
 <script setup>
 import { reactive } from 'vue'
 
-let initialState = reactive({ count: 0 })
+let initialState = reactive({ val: {count: 0}, user: ['HuXn', 'Jordan']})
 </script>
 
 <template>
-    <h1>Count: {{ initialState.count }}</h1>
-    <button @click=" initialState.count ++ " >+</button>
+   <h1>Current Count: {{ initialState.val.count }}</h1>
+   <h2>User: {{ initialState.user }}</h2>
+
+   <button @click="initialState.val.count+=10">Add 10</button>
+   <button>Change User</button>
+   <button>Add one more</button>
 </template>
