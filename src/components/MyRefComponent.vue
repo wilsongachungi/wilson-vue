1327e923d53829ae28e56d6e17ca15fd90a77a5d
wilsongachungi <gachungiwilson91@gmail.com>
2024-09-25 10:23:31 +0300

    using arrays

--- src/components/MyRefComponent.vue
+++ src/components/MyRefComponent.vue
@@ -1,7 +1,15 @@
 <script setup>
+import { ref } from 'vue'
 
+// let count = ref(0)
+
+let userName = ref('')
 </script>
 
 <template>
-    
+    <!-- <h1>Count: {{ count }}</h1>
+    <button @click="count++">+</button>
+    <button @click="count--">-</button> -->
+    <h1>Username: {{ userName }}</h1>
+    <button @click="userName = 'jordan'">Add user</button>
 </template>
