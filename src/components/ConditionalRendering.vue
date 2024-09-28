190343487c9f471e06f8ea3d1028614096a8c8c2
wilsongachungi <gachungiwilson91@gmail.com>
2024-09-28 11:25:33 +0300

    checking whether user is loged in to show details

--- src/components/ConditionalRendering.vue
+++ src/components/ConditionalRendering.vue
@@ -1,11 +1,18 @@
 <script setup>
-import { ref } from 'vue'
-let isVisible = ref(false)
+import { ref } from 'vue';
+
+let isUserLoggedIn = ref(true);
+let userName = 'wilson'
+let showDetails = ref(isUserLoggedIn);
+
 </script>
 
 <template>
     <div>
-       <p v-show="isVisible">This paragraph will be seen or hidden depending with the value of isVisible</p>
-       <button @click="isVisible = !isVisible">Toggle visibility</button>
+      <h1 v-if="isUserLoggedIn"> welcome {{  userName }} </h1>
+      <p v-else>please log in to see the content here.</p>
+    </div>
+    <div v-show="showDetails">
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. Aliquam error obcaecati rerum fugit quidem iusto vero repudiandae. Id, assumenda! Porro.
     </div>
 </template>
