8ec02827f4079c8479ed4495f6fb6da7f919ba9f
wilsongachungi <gachungiwilson91@gmail.com>
2024-09-28 12:56:32 +0300

    DE structuring

--- src/components/IterationComponent.vue
+++ src/components/IterationComponent.vue
@@ -1,9 +1,54 @@
 <script setup>
-    import { ref } from 'vue'
-    let peoples = ref(['wilson','john','damaris'])
+import { ref } from 'vue'
+let peoples = ref(['wilson', 'john', 'damaris'])
+
+const books = ([
+    {
+        index: 1,
+        title: "To Kill a Mockingbird",
+        author: "Harper Lee",
+        genre: "Fiction",
+        publicationYear: 1960
+    },
+    {
+        index: 2,
+        title: "1984",
+        author: "George Orwell",
+        genre: "Dystopian",
+        publicationYear: 1949
+    },
+    {
+        index: 3,
+        title: "The Great Gatsby",
+        author: "F. Scott Fitzgerald",
+        genre: "Classic",
+        publicationYear: 1925
+    },
+    {
+        index: 4,
+        title: "Harry Potter and the Sorcerer's Stone",
+        author: "J.K. Rowling",
+        genre: "Fantasy",
+        publicationYear: 1997
+    },
+    {
+        index: 5,
+        title: "The Catcher in the Rye",
+        author: "J.D. Salinger",
+        genre: "Literary Fiction",
+        publicationYear: 1951
+    }
+]);
+
 
 </script>
 
 <template>
-    <p v-for="(person, index) in peoples" :key="index">{{ person }}</p>
+ <ul v-for="({id, title, author, genre, publicationYear}, i) in books" :key="i">
+    <li>Id: {{ id }}</li>
+    <li>Title: {{ title }}</li>
+    <li>Author: {{ author }}</li>
+    <li>Genre: {{ genre }}</li>
+    <li>Publication year: {{ publicationYear }}</li>
+ </ul>
 </template>
