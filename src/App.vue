86958bae80abb0477f6153c0f82ee5d54f65cefd
wilsongachungi <gachungiwilson91@gmail.com>
2024-09-30 21:30:10 +0300

    passing data from child component to the parent

--- src/App.vue
+++ src/App.vue
@@ -2,35 +2,18 @@
 import './index.css';
 import { ref } from 'vue';
 
-// Defining reactive variables
-const firstName = ref('Wilson')
-const lastName = ref('WebDev')
-const MyFavoriteNumber = ref(7)
 
-// Importing your components
-import StaticPropsComponent from './components/StaticPropsComponent.vue';
-import DynamicPropsComponent from './components/DynamicPropsComponent.vue';
-import ChangingValueProps from './components/ChangingValueProps.vue';
-import PropsValidation from './components/PropsValidation.vue';
-import ComplexProps from './components/ComplexProps.vue';
-import CustomValidator from './components/CustomValidator.vue';
+import ComponentEvent from './components/ComponentEvent.vue';
+
+let count = ref(0)
 </script>
 
 <template>
-  <!-- Static and Dynamic Props Components -->
-  <StaticPropsComponent name="Wilson" />
-  <DynamicPropsComponent :firstName="firstName" :lastName="lastName" />
 
-  <hr>
 
-  <!-- Changing Value Props Component -->
-  <ChangingValueProps :MyFavoriteNumber="MyFavoriteNumber" />
-  <hr>
-  <PropsValidation fullName="wilson" :age="25" />
-  <hr>
-  <ComplexProps :friends="['alex', 'john', 'daidai']" :userInfo="{name: 'alex', age:20, location: ['ciaraini','nyeri']}" />
-  <hr>
-  <CustomValidator name="wilson" :age="26" password="impossiblePassword"/>
+  <h1>Count: {{ count }}</h1> 
+  
+<ComponentEvent @incrementCounter="count++"/>
 
 </template>
 
---
+++ src/components/ComponentEvent.vue
@@ -0,0 +1,7 @@
+<script setup>
+
+</script>
+
+<template>
+   <button @click="$emit('incrementCounter')">+</button>
+</template>
