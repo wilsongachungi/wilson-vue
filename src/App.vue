a2e20072579b15bec3334dbc133506ab793c14e9
wilsongachungi <gachungiwilson91@gmail.com>
2024-10-01 22:03:14 +0300

    practice for provide and inject method

--- src/App.vue
+++ src/App.vue
@@ -1,17 +1,60 @@
 <script setup>
+import {provide} from 'vue'
+import PracticeComponent from './components/PracticeComponent.vue';
 
-import SchoolComponent from './components/SchoolComponent.vue';
-import { provide } from 'vue'
+provide('friends',['alex','joram','Daidai'])
 
-provide('studentName', 'alex')
-provide('studentAge', '26')
-provide('studentLocation', ['ciaraini','nyeri'])
+provide('games', {
+  id:1,
+  title: "Epic adventure game",
+  genre: ['Action','Adventure', 'RPG'],
+  platform: ['pc','playstation','xbox'],
+  releasedate: '2023-05-16',
+  developer: {
+    name: 'owesome game studios',
+    location: 'Tokyo',
+    teamSize:63
+  }
+})
+
+provide('moreGames',[
+  {
+    title: "The Legend of Zelda: Breath of the Wild",
+    genre: "Action-adventure",
+    platform: ["Nintendo Switch", "Wii U"],
+    releaseDate: "March 3, 2017"
+  },
+  {
+    title: "God of War",
+    genre: "Action-adventure",
+    platform: ["PlayStation 4", "PlayStation 5"],
+    releaseDate: "April 20, 2018"
+  },
+  {
+    title: "Among Us",
+    genre: "Multiplayer, Social Deduction",
+    platform: ["PC", "Nintendo Switch", "iOS", "Android", "PlayStation", "Xbox"],
+    releaseDate: "June 15, 2018"
+  },
+  {
+    title: "Cyberpunk 2077",
+    genre: "Action role-playing",
+    platform: ["PC", "PlayStation 4", "PlayStation 5", "Xbox One", "Xbox Series X/S"],
+    releaseDate: "December 10, 2020"
+  },
+  {
+    title: "Minecraft",
+    genre: "Sandbox, Survival",
+    platform: ["PC", "Xbox", "PlayStation", "Nintendo Switch", "iOS", "Android"],
+    releaseDate: "November 18, 2011"
+  }
+])
 
 </script>
 
 <template>
 
-  <SchoolComponent />
+<PracticeComponent/>
  
 </template>
 
---
+++ src/components/PracticeComponent.vue
@@ -0,0 +1,39 @@
+<script setup>
+import { inject } from 'vue'
+
+const friendList = inject('friends')
+const games = inject('games')
+const moreGames = inject('moreGames')
+</script>
+
+<template>
+    <h1>Injecting Array</h1>
+    <ul v-for="(friend, index) in friendList" :key="index">
+        <li>{{ friend }}</li>
+    </ul>
+
+    <hr>
+
+    <h1>Injecting Object</h1>
+    <ul v-for="(game, i ) in games" :key="i">
+        <li>{{ game }}</li>
+    </ul>
+
+    <hr>
+    <h1>Array of objects</h1>
+    <ul v-for="(moreGame, i) in moreGames" :key="i">
+        <li>
+            {{ moreGame }}
+        </li>
+    </ul>
+
+    <hr>
+    <h1>Desructuring</h1>
+    <ul v-for="({title,genre,platform,releaseDate}, i) in moreGames" :key="i">
+        <li>Title:{{ title }}</li>
+        <li>Platform:{{ platform }}</li>
+        <li>Genre:{{ genre }}</li>
+        <li>Releasedate:{{ releaseDate }}</li>
+    </ul>
+
+</template>
