84c79d1b4919141df43c32b0e95f3ae6df16329b
wilsongachungi <gachungiwilson91@gmail.com>
2024-10-01 18:59:55 +0300

    two ways of calling a slot

--- src/App.vue
+++ src/App.vue
@@ -4,6 +4,7 @@ import { ref } from 'vue';
 
 import SlotComponent from './components/SlotComponent.vue';
 import FallbackContent from './components/FallbackContent.vue';
+import NamedSlot from './components/NamedSlot.vue'
 
 </script>
 
@@ -18,6 +19,19 @@ import FallbackContent from './components/FallbackContent.vue';
   <FallbackContent>
     <p>Component will not show because we are calling the paragraph direcly</p>
   </FallbackContent>
+
+  <hr>
+
+  <NamedSlot>
+    <template v-slot:one>
+      <h1>This content will go to slot 'one'</h1>
+    </template>
+
+    <template #two>
+      <h1>This content will go to slot 'two'</h1>
+    </template>
+  </NamedSlot>
+
 
 
 </template>
---
+++ src/components/NamedSlot.vue
@@ -0,0 +1,8 @@
+<script setup>
+
+</script>
+
+<template>
+    <slot name="one"></slot>
+    <slot name="two"></slot>
+</template>
