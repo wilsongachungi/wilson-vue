ace605172c79c3bba23af4b86c2bb86b9a1dcbcf
wilsongachungi <gachungiwilson91@gmail.com>
2024-10-02 09:45:27 +0300

    lifeCycle component

--- src/App.vue
+++ src/App.vue
@@ -1,60 +1,14 @@
 <script setup>
-import {provide} from 'vue'
-import PracticeComponent from './components/PracticeComponent.vue';
-
-provide('friends',['alex','joram','Daidai'])
-
-provide('games', {
-  id:1,
-  title: "Epic adventure game",
-  genre: ['Action','Adventure', 'RPG'],
-  platform: ['pc','playstation','xbox'],
-  releasedate: '2023-05-16',
-  developer: {
-    name: 'owesome game studios',
-    location: 'Tokyo',
-    teamSize:63
-  }
-})
-
-provide('moreGames',[
-  {
-    title: "The Legend of Zelda: Breath of the Wild",
-    genre: "Action-adventure",
-    platform: ["Nintendo Switch", "Wii U"],
-    releaseDate: "March 3, 2017"
-  },
-  {
-    title: "God of War",
-    genre: "Action-adventure",
-    platform: ["PlayStation 4", "PlayStation 5"],
-    releaseDate: "April 20, 2018"
-  },
-  {
-    title: "Among Us",
-    genre: "Multiplayer, Social Deduction",
-    platform: ["PC", "Nintendo Switch", "iOS", "Android", "PlayStation", "Xbox"],
-    releaseDate: "June 15, 2018"
-  },
-  {
-    title: "Cyberpunk 2077",
-    genre: "Action role-playing",
-    platform: ["PC", "PlayStation 4", "PlayStation 5", "Xbox One", "Xbox Series X/S"],
-    releaseDate: "December 10, 2020"
-  },
-  {
-    title: "Minecraft",
-    genre: "Sandbox, Survival",
-    platform: ["PC", "Xbox", "PlayStation", "Nintendo Switch", "iOS", "Android"],
-    releaseDate: "November 18, 2011"
-  }
-])
+import { ref } from 'vue'
+import LifecycleComponent from './components/LifecycleComponent.vue';
+const showHide = ref(true)
 
 </script>
 
 <template>
 
-<PracticeComponent/>
+<LifecycleComponent v-if="showHide"/>
+<button @click="showHide = !showHide">Show / Hide</button>
  
 </template>
 
---
+++ src/components/LifecycleComponent.vue
@@ -0,0 +1,40 @@
+<script setup>
+import { ref, onBeforeMount, onMounted, onBeforeUpdate, onUpdated, onBeforeUnmount, onUnmounted } from 'vue'
+
+const message = ref('hello, vue!')
+
+// lifecycle hooks
+onBeforeMount(() => {
+    console.log('before mount')
+})
+
+onMounted(() => {
+    console.log('mounted')
+})
+
+onBeforeUpdate(() => {
+    console.log('before updated')
+})
+
+onBeforeUnmount(() => {
+    console.log('component is about to be unmounted')
+})
+
+const updateMessage = () => {
+   message.value = 'updated message' 
+}
+
+onUnmounted(() => {
+    console.log('component has been unmounted')
+})
+
+onUpdated(() => {
+    console.log('component has been updated')
+})
+
+</script>
+
+<template>
+    <p>{{ message }}</p>
+    <button @click="updateMessage">Update message</button>
+</template>
