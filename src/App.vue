9764aedbc00eff17b5961a5e9bc43a02277b0593
wilsongachungi <gachungiwilson91@gmail.com>
2024-10-02 10:08:15 +0300

    using watch

--- src/App.vue
+++ src/App.vue
@@ -1,14 +1,13 @@
 <script setup>
+
 import { ref } from 'vue'
-import LifecycleComponent from './components/LifecycleComponent.vue';
-const showHide = ref(true)
+import BasicComponent from './components/BasicComponent.vue';
 
 </script>
 
 <template>
 
-<LifecycleComponent v-if="showHide"/>
-<button @click="showHide = !showHide">Show / Hide</button>
+    <BasicComponent/>
  
 </template>
 
---
+++ src/components/BasicComponent.vue
@@ -0,0 +1,20 @@
+<script setup>
+import { ref, watch } from 'vue'
+const message = ref('hello, vue!')
+const inputText = ref('apple')
+
+//wach for changes in input text and upodate message accordingly
+watch(inputText,(newValue, oldValue) => {
+    //you can perform some logics here when input text changes
+    console.log(newValue)
+    console.log(oldValue)
+    message.value = `You Typed: ${newValue}`
+})
+</script>
+
+<template>
+   <div>
+    <h1>{{ message }}</h1>
+    <input type="text" v-model="inputText" placeholder="type something...">
+   </div>
+</template>
